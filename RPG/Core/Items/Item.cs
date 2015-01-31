33e101a32d9567f68eeb845eb560bc7267e45ae2
RkTrail <finalfantasy-magister@hotmail.fr>
2015-01-31 17:28:27 +0100

    create object item

---
+++ RPG/Core/Items/Item.cs
@@ -0,0 +1,24 @@
+﻿namespace RPG.Core.Items
+{
+    class Item
+    {
+        protected string _nom;
+        protected string _description;
+
+        public string Nom
+        {
+            get { return _nom; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+        }
+
+        protected Item(string nom, string description)
+        {
+            _nom = nom;
+            _description = description;
+        }
+    }
+}
