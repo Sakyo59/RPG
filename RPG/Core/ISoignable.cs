60d7444de03e7b4e3358d8cb39473117db23f064
Sakyo59 <renato.duriez@gmail.com>
2015-01-31 17:22:24 +0100

    add IAttaquable and ISoignable

---
+++ RPG/Core/ISoignable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPG.Core
+{
+    interface ISoignable
+    {
+        void Soigner(int PvGagner;
+    }
+}
