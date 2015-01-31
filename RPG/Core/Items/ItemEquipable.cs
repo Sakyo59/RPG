a518102a3790c29ba9f48dd1454f56471af12098
RkTrail <finalfantasy-magister@hotmail.fr>
2015-01-31 18:13:07 +0100

    add itemUtilisable and add type to ItemEquipable

--- RPG/Core/Items/ItemEquipable.cs
+++ RPG/Core/Items/ItemEquipable.cs
@@ -7,32 +7,33 @@ using OpenTK.Graphics.OpenGL;
 
 namespace RPG.Core.Items
 {
+    public enum TypeItemEquipable
+    {
+        Tete,
+        Torse,
+        Jamble,
+        Pied,
+        Arme1Main,
+        Arme2Main,
+        Bouclier
+    }
     class ItemEquipable : Item
     {
-        public enum Type
-        {
-            Tete,
-            Torse,
-            Jamble,
-            Pied,
-            Arme1Main,
-            Arme2Main,
-            Bouclier
-        }
-
+        private TypeItemEquipable _typeItemEquipable;
         private int _pv;
         private int _mana;
         private int _degatPhysique;
         private int _degatMagique;
         private int _armure;
 
-        public ItemEquipable(string nom, string description, int pv, int mana, int degatPhysique, int degatMagique, int armure) : base(nom, description)
+        public ItemEquipable(string nom, string description, int pv, int mana, int degatPhysique, int degatMagique, int armure, TypeItemEquipable typeItemEquipable) : base(nom, description)
         {
             _pv = pv;
             _mana = mana;
             _degatPhysique = degatPhysique;
             _degatMagique = degatMagique;
             _armure = armure;
+            _typeItemEquipable = typeItemEquipable;
         }
     }
 }
---
+++ RPG/Core/Items/ItemUtilisable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Graphics.OpenGL;
+
+namespace RPG.Core.Items
+{
+    public enum TypeItemUtilisable
+    {
+        soin,
+        degat
+    }
+    class ItemUtilisable : Item
+    {
+        
+
+        private TypeItemUtilisable _typeItemUtilisable;
+        private int _pv;
+        private int _degat;
+
+        public TypeItemUtilisable TypeItemUtilisable
+        {
+            get { return _typeItemUtilisable; }
+        }
+
+        public int Pv
+        {
+            get { return _pv;}
+        }
+
+        public int Degat
+        {
+            get { return _degat; }
+        }
+
+    }
+}
