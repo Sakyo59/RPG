aca73941a9241a11db5fb8cf2936ff3f0c423204
Sakyo59 <renato.duriez@gmail.com>
2015-01-31 17:14:23 +0100

    Add Joueur

---
+++ RPG/Core/Joueur.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPG.Core
+{
+    class Joueur:Entity
+    {
+        private int _degatPhysique;
+        private int _degatMagique;
+
+        public int DegatPhysique
+        {
+            get
+            {
+                return _degatPhysique;
+            }
+        }
+
+        public int DegatMagique
+        {
+            get
+            {
+                return _degatMagique;
+            }
+        }
+
+    }
+}
