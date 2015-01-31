69371ef77a207d7862363ae5bb0102c3abe616d9
Sakyo59 <renato.duriez@gmail.com>
2015-01-31 18:11:35 +0100

    Add Joueur.constructeur and Ennemie

---
+++ RPG/Core/Ennemie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPG.Core
+{
+    class Ennemie:Entity,ISoignable,IAttaquable
+    {
+        public void Soigner(int PvGagner)
+        {
+            _pvActuel = _pvActuel + PvGagner;
+        }
+
+
+        public void PrendreDegats(int DegatsSubis)
+        {
+            _pvActuel = _pvActuel - DegatsSubis;
+        }
+
+        public Ennemie(int pvMax, int manaMax, int force, int intel, int armure, string nom, int level, int degatPhysique, int degatMagique)
+            : base(pvMax, manaMax, force, intel, armure, nom, level)
+        {
+
+        }
+    }
+}
--- RPG/Core/Joueur.cs
+++ RPG/Core/Joueur.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace RPG.Core
 {
-    class Joueur:Entity
+    class Joueur:Entity,IAttaquable,ISoignable
     {
         private int _degatPhysique;
         private int _degatMagique;
@@ -26,5 +26,22 @@ namespace RPG.Core
             }
         }
 
+
+        public void Soigner(int PvGagner)
+        {
+            _pvActuel = _pvActuel + PvGagner;
+        }
+
+        public void PrendreDegats(int DegatsSubis)
+        {
+            _pvActuel = _pvActuel - DegatsSubis;
+        }
+
+        public Joueur(int pvMax, int manaMax, int force, int intel, int armure, string nom, int level, int degatPhysique, int degatMagique)
+            : base(pvMax, manaMax, force, intel, armure, nom, level)
+        {
+            _degatMagique = degatMagique;
+            _degatPhysique = degatPhysique;
+        }
     }
 }
